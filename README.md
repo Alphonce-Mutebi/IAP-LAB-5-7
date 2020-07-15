197fc43d516ac60b461b283b33d1a605fae2d2da
Alphonce-Mutebi <mutebialphonce@gmail.com>
2020-07-16 00:08:44 +0300

    lab 5-7

---
+++ README.md
@@ -0,0 +1,10 @@
+# ICS 3104: Internet Application Programming -Lab 5-7
+
+This repository contains code in fulfilment of ICS 3104 Course Work.
+
+### Setup
+
+-   Clone and place in htdocs/. Install requirements and migrate Database.
+-   Test API: use run in postman button in welcome page or import using postman collection file in root of folder.
+
+_Student: ICS: C ~ 110605._
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Introduction to Laravel
+                </div>
+                <h4>Login/Register to continue...</h4>
+
+                <h5>API postman collection</h5>
+                    <div class="postman-run-button"
+                    data-postman-action="collection/import"
+                    data-postman-var-1="5276f0d0b1032d5c6c3d"></div>
+                    <script type="text/javascript">
+                    (function (p,o,s,t,m,a,n) {
+                        !p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });
+                        !o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild((
+                        (n = o.createElement("script")),
+                        (n.id = s+t), (n.async = 1), (n.src = m), n
+                        ));
+                    }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+                    </script>
+            </div>
+        </div>
+    </body>
+</html>
